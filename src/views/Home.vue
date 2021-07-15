a9c1538a5b74a8da233872a781393280b4e18294
MirajHoque <a.h.miraj2031@gmail.com>
2021-07-15 21:46:16 +0600

    Template Refs
    Reference

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,36 @@
 <template>
   <div class="home">
     home
-    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
     <button @click="handleClick">Click Me</button>
   </div>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity';
 
 export default {
   name: 'Home',
   setup() {
-    console.log('setup')
+    console.log(this)
+
+    const p= ref('hello');
+    console.log(p);
 
     let name = 'mario';
     let age = 30;
 
      const handleClick= ()=> {
-      console.log('you clicked me');
+      console.log(p, p.value);
+      p.value.classList.add("test");
+      p.value.textContent = 'Hello, ninjas'
     } 
 
     //return { name: name, age: age};
     //if name of key and name of the variable assigning that  are same we shorthand this
     //return { name, age}
 
-    return{ name, age, handleClick}
+    return{ name, age, handleClick,p}
 
    
   },
