eb64d67dfea7a2e78570aa8059adc5f5199eea7a
MirajHoque <a.h.miraj2031@gmail.com>
2021-07-16 22:59:42 +0600

    watch and watchEffect

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,26 +3,41 @@
     <h1>Home</h1>
     <input type="text" v-model="search">
     <p>search term - {{ search }}</p>
+
     <div v-for="element in matchingSearch" :key="element">
       <p>{{ element }}</p>
     </div>
+    
+     <button @click="handleClick">stop watching</button>
   </div>
 </template>
 
 <script>
-import { computed, ref } from '@vue/runtime-core'
+import { computed, ref, watch, watchEffect } from '@vue/runtime-core'
 
 export default {
   name: 'Home',
   setup() {
     const search = ref('');
     const names = ref(['mario', 'yoshi', 'luigi', 'toad', 'bowser', 'koopa', 'peach'])
+    
+    const stopWatch = watch(search, () => {
+      console.log('watch function run');
+    })
+    const stopWatchEffec= watchEffect( ()=> {
+      console.log('watchEffect function run', search.value);
+    })
 
     const matchingSearch =  computed( () => {
       return names.value.filter((element) => element.includes(search.value));
     })
 
-     return { names, search, matchingSearch }
+    const handleClick = () => {
+      stopWatch();
+      stopWatchEffec()
+    }
+
+     return { names, search, matchingSearch, handleClick }
   }
  
 }
