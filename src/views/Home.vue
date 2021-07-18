f48cc88da83f5568c54bcaabbc5cb725a501a153
MirajHoque <a.h.miraj2031@gmail.com>
2021-07-18 23:10:24 +0600

    fetching data inside setup function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <PostLists v-if="showPosts" :posts="posts" />
-    <button @click=" showPosts=!showPosts">show posts</button>
-    <button @click="posts.pop()">delete a post</button>
+
+    <div v-if="error"> {{ error }}</div>
+
+    <div v-if="posts.length">
+      <PostLists :posts="posts" />
+    </div>
+
+    <div v-else>Loading....</div>
   </div>
 </template>
 
@@ -15,13 +20,25 @@ export default {
   name: 'Home',
   components: {PostLists},
   setup() {
-    const posts= ref([
-      {title: 'welcome to the blog', body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt ad sed veniam possimus quo harum soluta at vero! Ducimus reiciendis ea nostrum quos minus quas dicta laudantium sint, enim laborum recusandae eveniet cumque quasi perspiciatis voluptates vero iure molestias mollitia aperiam, harum explicabo sit repellendus repellat itaque? Doloremque deleniti adipisci odio voluptatibus vel! Obcaecati, dolorem optio inventore aperiam doloremque cupiditate vitae debitis alias consectetur accusamus placeat eos porro voluptatibus dolorum incidunt fugiat eveniet illum voluptatum autem in asperiores? Quasi tempore officia enim iusto saepe quam, tenetur possimus, dolores ipsam qui magnam. Quidem, quos maiores adipisci non fugit blanditiis veniam soluta! Recusandae minima amet magni? Harum vel, eveniet nam ducimus possimus quis in fuga ab dolores perspiciatis suscipit delectus ipsum quisquam quidem similique necessitatibus at incidunt vitae porro quo unde odit. Saepe nihil velit dolorum iure, quos neque animi ipsa nam aliquam, aperiam autem. Voluptatem repellendus iure in quidem, omnis officia!', id: 1},
-      {title: 'top 5 css tips', body: 'lorem ipsum', id: 2}
-    ])
-    const showPosts = ref(true)
+    const posts= ref([])
+    const error = ref(null)
+    const load = async () => {
+      try {
+        let data = await fetch(' http://localhost:3000/posts')
+        if(!data.ok) {
+          throw Error('no data availabe')
+        }
+        posts.value = await data.json()
+      }
+      catch(err) {
+        error.value = err.message;
+        console.log(error.value);
+      }
 
-     return { posts, showPosts }
+    }
+    load()
+
+     return { posts, error }
      
   }
  
