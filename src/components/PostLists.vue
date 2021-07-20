7513c5631a15b1fda73fca1d9e40b7f155655d74
MirajHoque <a.h.miraj2031@gmail.com>
2021-07-20 22:20:40 +0600

    reuseable composables
    compositon function

---
+++ src/components/PostLists.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="post-lists">
+    <div v-for="element in posts" :key="element.id">
+      <SinglePost :post="element" />
+    </div>
+  </div>
+</template>
+ 
+<script>
+import { onMounted, onUnmounted, onUpdated } from '@vue/runtime-core'
+import SinglePost from '../components/SinglePost.vue'
+
+export default {
+  name: 'PostLists',
+  components: { SinglePost },
+  props: ['posts'],
+
+  //using props inside setup function
+  setup(props){
+    onMounted( () => console.log('component mounted'));
+    onUnmounted( () => console.log('component unmounted'));
+    onUpdated( () => console.log('component updated'));
+
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/SinglePost.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="post">
+    <router-link :to="{ name: 'Details', params: { id: post.id}}">
+      <h3>{{ post.title }}</h3>
+    </router-link>
+    <h3>{{ snippet }}</h3>
+    <span v-for="element in post.tags" :key="element">
+      #{{ element }}
+    </span>
+  </div>
+</template>
+
+<script>
+import { computed } from '@vue/runtime-core'
+export default {
+  name: 'SinglePost',
+  props: ['post'],
+  setup(props) {
+    const snippet= computed( () => {
+      return props.post.body.substring(0,100) + '...';
+    })
+
+    return { snippet }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
